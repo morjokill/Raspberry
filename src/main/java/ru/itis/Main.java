7bdb9f3691465fcb2348d03bf877dc65114dcce9
romanovmv <morjokill@gmail.com>
2019-03-01 15:38:13 +0300

    Add imperial march

--- src/main/java/ru/itis/Main.java
+++ src/main/java/ru/itis/Main.java
@@ -4,7 +4,13 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
+import static com.pi4j.wiringpi.Gpio.delay;
+import static com.pi4j.wiringpi.Gpio.delayMicroseconds;
+import static ru.itis.NoteConstants.*;
+
 public class Main {
+    private static boolean isPlaying = false;
+
     public static void main(String[] args) {
         final GpioController controller = GpioFactory.getInstance();
 
@@ -20,11 +26,139 @@ public class Main {
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                 System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = "
                         + event.getState());
+
                 output.pulse(1000);
+
+                if (!isPlaying) {
+                    isPlaying = true;
+                    play(output);
+                } else {
+                    isPlaying = false;
+                }
             }
         });
 
         output.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
         input.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+    }
+
+    private static void beep(int note, int duration, GpioPinDigitalOutput output) throws Exception {
+        if (isPlaying) {
+            //This is the semiperiod of each note.
+            long beepDelay = (long) (1000000 / note);
+            //This is how much time we need to spend on the note.
+            long time = (duration * 1000) / (beepDelay * 2);
+            for (int i = 0; i < time; i++) {
+                //1st semiperiod
+                output.setState(PinState.HIGH);
+                delayMicroseconds(beepDelay);
+                //2nd semiperiod
+                output.setState(PinState.LOW);
+                delayMicroseconds(beepDelay);
+            }
+
+            //Add a little delay to separate the single notes
+            output.setState(PinState.LOW);
+            delay(20);
+        } else {
+            throw new Exception("Stop playing");
+        }
+    }
+
+    private static void play(GpioPinDigitalOutput output) {
+        while (isPlaying) {
+            try {
+                beep(a, 500, output);
+                beep(a, 500, output);
+                beep(f, 350, output);
+                beep(cH, 150, output);
+
+                beep(a, 500, output);
+                beep(f, 350, output);
+                beep(cH, 150, output);
+                beep(a, 1000, output);
+                beep(eH, 500, output);
+
+                beep(eH, 500, output);
+                beep(eH, 500, output);
+                beep(fH, 350, output);
+                beep(cH, 150, output);
+                beep(gS, 500, output);
+
+                beep(f, 350, output);
+                beep(cH, 150, output);
+                beep(a, 1000, output);
+                beep(aH, 500, output);
+                beep(a, 350, output);
+
+                beep(a, 150, output);
+                beep(aH, 500, output);
+                beep(gHS, 250, output);
+                beep(gH, 250, output);
+                beep(fHS, 125, output);
+
+                beep(fH, 125, output);
+                beep(fHS, 250, output);
+
+                delay(250);
+
+                beep(aS, 250, output);
+                beep(dHS, 500, output);
+                beep(dH, 250, output);
+                beep(cHS, 250, output);
+                beep(cH, 125, output);
+
+                beep(b, 125, output);
+                beep(cH, 250, output);
+
+                delay(250);
+
+                beep(f, 125, output);
+                beep(gS, 500, output);
+                beep(f, 375, output);
+                beep(a, 125, output);
+                beep(cH, 500, output);
+
+                beep(a, 375, output);
+                beep(cH, 125, output);
+                beep(eH, 1000, output);
+                beep(aH, 500, output);
+                beep(a, 350, output);
+
+                beep(a, 150, output);
+                beep(aH, 500, output);
+                beep(gHS, 250, output);
+                beep(gH, 250, output);
+                beep(fHS, 125, output);
+
+                beep(fH, 125, output);
+                beep(fHS, 250, output);
+
+                delay(250);
+
+                beep(aS, 250, output);
+                beep(dHS, 500, output);
+                beep(dH, 250, output);
+                beep(cHS, 250, output);
+                beep(cH, 125, output);
+
+                beep(b, 125, output);
+                beep(cH, 250, output);
+
+                delay(250);
+
+                beep(f, 250, output);
+                beep(gS, 500, output);
+                beep(f, 375, output);
+                beep(cH, 125, output);
+                beep(a, 500, output);
+
+                beep(f, 375, output);
+                beep(c, 125, output);
+                beep(a, 1000, output);
+            } catch (Exception e) {
+                System.out.println("Stopping playing: " + e);
+            }
+        }
     }
 }
---
+++ src/main/java/ru/itis/NoteConstants.java
@@ -0,0 +1,42 @@
+package ru.itis;
+
+public class NoteConstants {
+    public static final int cL = 129;
+    public static final int cLS = 139;
+    public static final int dL = 146;
+    public static final int dLS = 156;
+    public static final int eL = 163;
+    public static final int fL = 173;
+    public static final int fLS = 185;
+    public static final int gL = 194;
+    public static final int gLS = 207;
+    public static final int aL = 219;
+    public static final int aLS = 228;
+    public static final int bL = 232;
+
+    public static final int c = 261;
+    public static final int cS = 277;
+    public static final int d = 294;
+    public static final int dS = 311;
+    public static final int e = 329;
+    public static final int f = 349;
+    public static final int fS = 370;
+    public static final int g = 391;
+    public static final int gS = 415;
+    public static final int a = 440;
+    public static final int aS = 455;
+    public static final int b = 466;
+
+    public static final int cH = 523;
+    public static final int cHS = 554;
+    public static final int dH = 587;
+    public static final int dHS = 622;
+    public static final int eH = 659;
+    public static final int fH = 698;
+    public static final int fHS = 740;
+    public static final int gH = 784;
+    public static final int gHS = 830;
+    public static final int aH = 880;
+    public static final int aHS = 910;
+    public static final int bH = 933;
+}
