274beac13c1eb24ccf84e0e4d2998bc5f4214b54
morjokill <morjokill@gmail.com>
2019-04-04 01:45:38 +0300

    Change post mapping

--- song-player/src/main/java/ru/itis/controller/SongController.java
+++ song-player/src/main/java/ru/itis/controller/SongController.java
@@ -6,7 +6,6 @@ import ru.itis.model.Song;
 import ru.itis.model.web.SongsResponse;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/")
@@ -38,9 +37,9 @@ public class SongController {
     }
 
     @POST
-    @Path("/songs")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/post")
     public Response changeCurrentSong(int currentSongId) {
+        System.out.println("CurrentSongId from post: " + currentSongId);
         Main.songPlayer.setCurrentSong(currentSongId);
         return Response.status(200).entity(currentSongId).build();
     }
