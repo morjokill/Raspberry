075dd487fa5ec21dd8d614cf38e0a880acb76f83
morjokill <morjokill@gmail.com>
2019-04-04 02:23:37 +0300

    Change Mario theme schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -60,6 +60,96 @@ INSERT INTO note (name, duration) VALUES ('aH', 880);
 INSERT INTO note (name, duration) VALUES ('aHS', 910);
 INSERT INTO note (name, duration) VALUES ('bH', 933);
 
+INSERT INTO note (name, duration) VALUES ('B0', 31)
+INSERT INTO note (name, duration) VALUES ('C1', 33)
+INSERT INTO note (name, duration) VALUES ('CS1', 35)
+INSERT INTO note (name, duration) VALUES ('D1', 37)
+INSERT INTO note (name, duration) VALUES ('DS1', 39)
+INSERT INTO note (name, duration) VALUES ('E1', 41)
+INSERT INTO note (name, duration) VALUES ('F1', 44)
+INSERT INTO note (name, duration) VALUES ('FS1', 46)
+INSERT INTO note (name, duration) VALUES ('G1', 49)
+INSERT INTO note (name, duration) VALUES ('GS1', 52)
+INSERT INTO note (name, duration) VALUES ('A1', 55)
+INSERT INTO note (name, duration) VALUES ('AS1', 58)
+INSERT INTO note (name, duration) VALUES ('B1', 62)
+INSERT INTO note (name, duration) VALUES ('C2', 65)
+INSERT INTO note (name, duration) VALUES ('CS2', 69)
+INSERT INTO note (name, duration) VALUES ('D2', 73)
+INSERT INTO note (name, duration) VALUES ('DS2', 78)
+INSERT INTO note (name, duration) VALUES ('E2', 82)
+INSERT INTO note (name, duration) VALUES ('F2', 87)
+INSERT INTO note (name, duration) VALUES ('FS2', 93)
+INSERT INTO note (name, duration) VALUES ('G2', 98)
+INSERT INTO note (name, duration) VALUES ('GS2', 104)
+INSERT INTO note (name, duration) VALUES ('A2', 110)
+INSERT INTO note (name, duration) VALUES ('AS2', 117)
+INSERT INTO note (name, duration) VALUES ('B2', 123)
+INSERT INTO note (name, duration) VALUES ('C3', 131)
+INSERT INTO note (name, duration) VALUES ('CS3', 139)
+INSERT INTO note (name, duration) VALUES ('D3', 147)
+INSERT INTO note (name, duration) VALUES ('DS3', 156)
+INSERT INTO note (name, duration) VALUES ('E3', 165)
+INSERT INTO note (name, duration) VALUES ('F3', 175)
+INSERT INTO note (name, duration) VALUES ('FS3', 185)
+INSERT INTO note (name, duration) VALUES ('G3', 196)
+INSERT INTO note (name, duration) VALUES ('GS3', 208)
+INSERT INTO note (name, duration) VALUES ('A3', 220)
+INSERT INTO note (name, duration) VALUES ('AS3', 233)
+INSERT INTO note (name, duration) VALUES ('B3', 247)
+INSERT INTO note (name, duration) VALUES ('C4', 262)
+INSERT INTO note (name, duration) VALUES ('CS4', 277)
+INSERT INTO note (name, duration) VALUES ('D4', 294)
+INSERT INTO note (name, duration) VALUES ('DS4', 311)
+INSERT INTO note (name, duration) VALUES ('E4', 330)
+INSERT INTO note (name, duration) VALUES ('F4', 349)
+INSERT INTO note (name, duration) VALUES ('FS4', 370)
+INSERT INTO note (name, duration) VALUES ('G4', 392)
+INSERT INTO note (name, duration) VALUES ('GS4', 415)
+INSERT INTO note (name, duration) VALUES ('A4', 440)
+INSERT INTO note (name, duration) VALUES ('AS4', 466)
+INSERT INTO note (name, duration) VALUES ('B4', 494)
+INSERT INTO note (name, duration) VALUES ('C5', 523)
+INSERT INTO note (name, duration) VALUES ('CS5', 554)
+INSERT INTO note (name, duration) VALUES ('D5', 587)
+INSERT INTO note (name, duration) VALUES ('DS5', 622)
+INSERT INTO note (name, duration) VALUES ('E5', 659)
+INSERT INTO note (name, duration) VALUES ('F5', 698)
+INSERT INTO note (name, duration) VALUES ('FS5', 740)
+INSERT INTO note (name, duration) VALUES ('G5', 784)
+INSERT INTO note (name, duration) VALUES ('GS5', 831)
+INSERT INTO note (name, duration) VALUES ('A5', 880)
+INSERT INTO note (name, duration) VALUES ('AS5', 932)
+INSERT INTO note (name, duration) VALUES ('B5', 988)
+INSERT INTO note (name, duration) VALUES ('C6', 1047)
+INSERT INTO note (name, duration) VALUES ('CS6', 1109)
+INSERT INTO note (name, duration) VALUES ('D6', 1175)
+INSERT INTO note (name, duration) VALUES ('DS6', 1245)
+INSERT INTO note (name, duration) VALUES ('E6', 1319)
+INSERT INTO note (name, duration) VALUES ('F6', 1397)
+INSERT INTO note (name, duration) VALUES ('FS6', 1480)
+INSERT INTO note (name, duration) VALUES ('G6', 1568)
+INSERT INTO note (name, duration) VALUES ('GS6', 1661)
+INSERT INTO note (name, duration) VALUES ('A6', 1760)
+INSERT INTO note (name, duration) VALUES ('AS6', 1865)
+INSERT INTO note (name, duration) VALUES ('B6', 1976)
+INSERT INTO note (name, duration) VALUES ('C7', 2093)
+INSERT INTO note (name, duration) VALUES ('CS7', 2217)
+INSERT INTO note (name, duration) VALUES ('D7', 2349)
+INSERT INTO note (name, duration) VALUES ('DS7', 2489)
+INSERT INTO note (name, duration) VALUES ('E7', 2637)
+INSERT INTO note (name, duration) VALUES ('F7', 2794)
+INSERT INTO note (name, duration) VALUES ('FS7', 2960)
+INSERT INTO note (name, duration) VALUES ('G7', 3136)
+INSERT INTO note (name, duration) VALUES ('GS7', 3322)
+INSERT INTO note (name, duration) VALUES ('A7', 3520)
+INSERT INTO note (name, duration) VALUES ('AS7', 3729)
+INSERT INTO note (name, duration) VALUES ('B7', 3951)
+INSERT INTO note (name, duration) VALUES ('C8', 4186)
+INSERT INTO note (name, duration) VALUES ('CS8', 4435)
+INSERT INTO note (name, duration) VALUES ('D8', 4699)
+INSERT INTO note (name, duration) VALUES ('DS8', 4978)
+
 INSERT INTO note (name, duration) VALUES ('DELAY', 0);
 
 INSERT INTO sound (note, delay, song_id) VALUES ('a', 500, 1);
@@ -181,4 +271,89 @@ INSERT INTO sound (note, delay, song_id) VALUES ('d', 200, 2);
 INSERT INTO sound (note, delay, song_id) VALUES ('e', 200, 2);
 INSERT INTO sound (note, delay, song_id) VALUES ('d', 400, 2);
 INSERT INTO sound (note, delay, song_id) VALUES ('g', 400, 2);
+
+INSERT INTO song (name) VALUES ('Mario theme');
+
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('C7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+
+INSERT INTO sound (note, delay, song_id) VALUES ('C7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('A6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('B6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('AS6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('A6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+
+INSERT INTO sound (note, delay, song_id) VALUES ('G6', 110, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 110, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G7', 110, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('A7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('F7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('C7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('D7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('B6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+
+INSERT INTO sound (note, delay, song_id) VALUES ('C7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('A6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('B6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('AS6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('A6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+
+INSERT INTO sound (note, delay, song_id) VALUES ('G6', 110, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 110, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G7', 110, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('A7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('F7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('G7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('E7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('C7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('D7', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('B6', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
+INSERT INTO sound (note, delay, song_id) VALUES ('DELAY', 80, 3);
 
